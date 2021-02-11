2fb554ac3936f36fd65bb1eb30c0d6a80b60853a
JiSeonNam <i987412563i@gmail.com>
2021-02-11 23:14:56 +0900

    todo 복습

--- MomentumClone/todo.js
+++ MomentumClone/todo.js
@@ -1,73 +1,81 @@
+//to do list 함수
 const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
-const TODOS_LS = "toDos";
-let toDos = []; //toDoList에 추가될 경우 toDos에 array 데이터로 추가하기
+let arrList = []; //toDoList에 추가될 경우 array 데이터로 추가하기
 
+//원하는 list 지우기
+function delToDoList(event){
+    const delLi = event.target.parentNode; //delBtn의 부모 li 알아내기
+    toDoList.removeChild(delLi); //ul 태그의 자식노드에 해당하는 것 화면상에서 지우기
 
-
-//HTML에서 toDoList 지우기
-function deleteTodo(event){
-    const index = event.target.parentNode; //delete 버튼의 부모 li의 index 알아내기
-    toDoList.removeChild(index);
-    const cleanToDos = toDos.filter(function(toDo){
-        return toDo.id !== parseInt(li.id); //string을 숫자로 바꾸기
+    //localStorage에 저장한 특정 list 지우기
+    const delOneList = arrList.filter(function(element){
+        //arrList의 element 중 index값과 delLi li의 id값이 같지 않다면 true 반환
+        //delLi.id의 값을 string -> number로 변환, 데이터타입이 다를 경우 서로 다른 것으로 인식
+        return element.index !== parseInt(delLi.id);
     });
-    toDos = cleanToDos;
-    saveToDos(); //바뀐 toDoList 저장하기
+    arrList = delOneList; //현재 arrList에 delOneList를 대입한 뒤
+    saveToDoList(); //다시 localStorage에 저장
 }
 
-//toDos을 localStorage에 저장하기
-function saveToDos(toDoObj){
-    //Object 타입을 String으로 저장하기
-    localStorage.setItem("TODOS_LS", JSON.stringify(toDos));
+
+//입력한 값 localStorage에 저장하기
+function saveToDoList(toDoObj){
+    //Object -> string을 변환
+    localStorage.setItem("TODOLIST", JSON.stringify(arrList));
 }
 
-function paintToDo(text){
-    const li = document.createElement("li"); //element 생성
+//입력한 값 화면에 보여주는 함수
+function showList(text){
+    //ul 태그 안에 li 안에 button, span 함수 생성
+    const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    delBtn.addEventListener("click", delToDoList); //delete버튼 클릭이벤트 발생
     const span = document.createElement("span");
-    const newId = toDos.length + 1; 
+    const index = arrList.length + 1;
 
-    delBtn.innerText = "❌"; //버튼안에 이모티콘 넣기
-    delBtn.addEventListener("click", deleteTodo); //클릭이벤트 발생
-    span.innerText = text;
-    li.appendChild(span); //li container안에 span(text)와 delete버튼 넣기
-    li.appendChild(delBtn); 
-    li.id = newId;
-    toDoList.appendChild(li); //appendChild : toDoList의 자식노드리스트 중 마지막 자식으로 붙이기
+    delBtn.innerText = "❌"; //버튼안에 X표시
+    span.innerText = text; //span안에 입력값 넣기
+    //입력한 값은 li의 맨 마지막에 추가해야함
+    li.appendChild(span); 
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+    li.id = index; //li에 index번호 부여하기
+
+    //arrList에 Object 형식으로 넣기위해 만들기
     const toDoObj = {
-        text: text,
-        id: newId //li의 index번호를 주기 위해 
+        index: index, //li의 index
+        content: text //list의 내용
     };
-    toDos.push(toDoObj); //toDos에 toDoObj 넣기
-    saveToDos(toDoObj);
+    arrList.push(toDoObj); //arrList에 넣기
+    saveToDoList(toDoObj); //localStorage에 저장하기
 }
 
+//form 태그 이벤트처리 함수
 function handleSubmit(event){
-    event.preventDefault(); //누르면 값 사라지는 default값 없애기
-    const currentValue = toDoInput.value; //입력값 가져오기
-    paintToDo(currentValue); //입력값 함수에 넣기
+    event.preventDefault(); //form 태그 default 이벤트 없애기
+    const currentValue = toDoInput.value; //입력값 
+    showList(currentValue); //함수에 넣기
     toDoInput.value = ""; //입력값 초기화
 }
 
-//localStorage에서 TODOS_LS 가져오기
-function loadToDos(){
-    const loadedToDos = localStorage.getItem("TODOS_LS");
-    if(loadedToDos !== null){
-        //JSON을 string으로 가져오기
-        const parsedToDos = JSON.parse(loadedToDos);
-
-        //가져온 parsedToDos를 forEach로 주어진 함수 배열 요소 각각에 대해 실행하기
-        parsedToDos.forEach(function(loadList){
-            paintToDo(loadList.text); //string으로 가져온 것 중 text만 나열하기 위해 
+//localStorage에 to do list 가져오기
+function loadList(){
+    //JSON을 string으로 가져오기
+    const loadList = JSON.parse(localStorage.getItem("TODOLIST"));
+    if(loadList !== null){
+        loadList.forEach(function(element){ //forEach로 요소 나열하기
+            showList(element.content); //html에 content 표시, 만약 element만 기입할 경우 Object로만 표시
         });
     }
 }
 
-//html load시 실행함수
+//html load
 function init(){
-    loadToDos(); 
-    toDoForm.addEventListener("submit", handleSubmit); //form submit 할 경우 handleSubmit함수 실행
+    //저장된 to do list함수 불러오기
+    loadList();
+    //form 태그 이벤트 전송
+    toDoForm.addEventListener("submit", handleSubmit);
 }
 init();
